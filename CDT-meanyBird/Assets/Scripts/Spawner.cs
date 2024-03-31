b0103a1f9c918fa482b4d6b09f94bea90032b8a3
codeninjascf <105656685+codeninjascf@users.noreply.github.com>
2024-03-31 12:14:57 +0100

    meany bird
    done from stage 0-18 / added spikes not finished yet

---
+++ CDT-meanyBird/Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    //Object that we will attack to the script for spawning object
+    [Header("Spikes Object for controlling the game")]
+    public GameObject spikes;
+    //Height position of tthe spikes
+    [Header("Default Height")]
+    public float height;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
