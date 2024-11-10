4077b9699715c0d5106df03f56b30105eec29438
codeninjascf <cnchandlersford@gmail.com>
2024-11-10 12:52:23 +0000

    food frenzy p2
    on: save gamemanager script

--- Food frenzy crush/Assets/Scripts/GameManager.cs
+++ Food frenzy crush/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,23 +10,65 @@ public class GameManager : MonoBehaviour
 
     private bool _gameEnded;
 
+    public UIManager uiManager;
 
     void Start()
     {
         _gameEnded = false;
+
+        if (level.type == LevelType.Moves)
+        {
+            uiManager.criteriaInfoText.text = "MovesRemaining";
+        }
+        else if (level.type == LevelType.Time)
+        {
+            uiManager.criteriaInfoText.text = "Time Remaining";
+        }
+
+        uiManager.highScoreNumber.text = "Time Remaining:";
+        uiManager.gameOverMenu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(level.GameOver && gridManager.MoveComplete && !_gameEnded)
+        if (level.type == LevelType.Moves)
         {
-            _gameEnded = true;
+            uiManager.criteriaNumber.text = level.MovesRemaning.ToString();
+        }
+        else if (level.type == LevelType.Time)
+        {
+            double timeRemaining = Math.Max(level.TimeRemaining, 0);
+            TimeSpan time = TimeSpan.FromSeconds(timeRemaining);
+            uiManager.criteriaNumber.text = time.ToString(@"mm\:ss");
+        }
+        
+        int starsAchieved = level.StarsAchieved(gridManager.Score); 
+
+        uiManager.UpdateScores(gridManager.Score);
+        uiManager.UpdateStars(starsAchieved);
+
+        if (level.GameOver && gridManager.MoveComplete && !_gameEnded)
+            {
+                _gameEnded = true;
 
             gridManager.GameActive = false;
 
+            string gameOverText = starsAchieved switch
+            {
+                0 => "Try Again",
+                1 => "good Job",
+                2 => "Excellent",
+                3 => "Perfect"
+            };
+
+            uiManager.gameOverText.text = gameOverText;
+            uiManager.newHighScoreText.SetActive(gridManager.Score > level.HighScore);
+            uiManager.gameOverMenu.SetActive(true);
+
             level.UpdateHighScore(gridManager.Score);
-            level.UpdateStarsAchieved(gridManager.Score);
+                level.UpdateStarsAchieved(gridManager.Score);
+            }
         }
     }
 
