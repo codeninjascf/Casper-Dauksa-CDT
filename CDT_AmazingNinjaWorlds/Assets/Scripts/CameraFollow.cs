15efa8d785cfad7602045f5ba835122e79ddcbca
codeninjascf <cnchandlersford@gmail.com>
2025-01-26 12:46:33 +0000

    ANW part 3 finished
    not started part 4 yet

--- CDT_AmazingNinjaWorlds/Assets/Scripts/CameraFollow.cs
+++ CDT_AmazingNinjaWorlds/Assets/Scripts/CameraFollow.cs
@@ -1,27 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class CameraFollow : MonoBehaviour
 {
+    public GameManager gameManager;
     public Transform target;
     public float smoothingTime = .2f;
     private Vector3 _velocity;
     public Vector3 cameraOffset;
 
+    public float deathHeight = -2;
+
+    private bool _following;
+    private float _cameraHeight;
+
     void Start()
     {
+        _following = true;
+        _cameraHeight = transform.position.y -
+            Camera.main.ViewportToWorldPoint(Vector3.zero).y;
 
+        ResetView();
     }
 
     void Update()
     {
-        Vector3 targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+        _following = target.position.y > deathHeight;
+        if (target.gameObject.activeSelf && target.position.y <=
+            deathHeight - _cameraHeight)
+        {
+            gameManager.KillPlayer();
+        }
+        {
+            Vector3 targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
 
-        transform.position = Vector3.SmoothDamp(transform.position, targetPos,
-        ref _velocity, smoothingTime);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPos,
+            ref _velocity, smoothingTime);
 
-         targetPos = new Vector3(target.position.x, target.position.y,
-            transform.position.z) + cameraOffset;
+            targetPos = new Vector3(target.position.x, target.position.y,
+               transform.position.z) + cameraOffset;
+        }
+    }
+
+
+
+
+
+    public void ResetView()
+    {
+        transform.position = new Vector3(target.position.x,
+            target.position.y, transform.position.z) + cameraOffset;
+        _velocity = Vector3.zero;
     }
 }
--- CDT_AmazingNinjaWorlds/Assets/Scripts/PlayerController.cs
+++ CDT_AmazingNinjaWorlds/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameManager gameManager;
     public float moveSpeed = 5f;
     public float jumpForce = 10f;
 
@@ -12,29 +11,50 @@ public class PlayerController : MonoBehaviour
     public LayerMask whatIsGround;
 
     private bool _isGrounded;
+    private bool _enabled;
     private Rigidbody2D _rigidbody;
 
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _enabled = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (!_enabled) return;
         _isGrounded = Physics2D.Raycast(transform.position, Vector2.down,
             groundDistanceThreshold, whatIsGround);
-    
-        if(_isGrounded && Input.GetButtonDown("Jump"))
+
+        if (_isGrounded && Input.GetButtonDown("Jump"))
         {
-        _rigidbody.velocity = Vector2.up * jumpForce;
+            _rigidbody.velocity = Vector2.up * jumpForce;
         }
     }
 
     private void FixedUpdate()
     {
+        if (!_enabled) return;
         float movement = moveSpeed * Input.GetAxisRaw("Horizontal");
         _rigidbody.position += movement * Time.deltaTime * Vector2.right;
     }
 
+
+    public void Enable()
+    {
+        _enabled = true;
+    }
+    public void Disable()
+    {
+        _enabled = false;
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Hazard"))
+        {
+            gameManager.KillPlayer();
+        }
+    }
 }
