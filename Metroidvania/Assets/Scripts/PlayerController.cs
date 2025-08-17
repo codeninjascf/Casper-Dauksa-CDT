cb792934d76eb506b493ab0041027c4b27e94271
codeninjascf <105656685+codeninjascf@users.noreply.github.com>
2025-08-17 12:53:29 +0100

    Metroidvania
    on the player 16 just added teh Ground Check Point

---
+++ Metroidvania/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D theRB;
+    public float moveSpeed;
+    public float jumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.velocity.y);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
+    }
+}
