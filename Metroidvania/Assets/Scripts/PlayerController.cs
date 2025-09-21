0b86c6fcb33f5af8d99a831c4a2d4ab92adae8e9
codeninjascf <105656685+codeninjascf@users.noreply.github.com>
2025-09-21 12:54:38 +0100

    metroidvania
    on Part 2 - Player - step 26. Animation

--- Metroidvania/Assets/Scripts/PlayerController.cs
+++ Metroidvania/Assets/Scripts/PlayerController.cs
@@ -8,14 +8,19 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D theRB;
     public float moveSpeed;
     public float jumpForce;
+    public Transform groundPoint;
+    private bool isOnGround;
+    public LayerMask WhatIsGround;
 
     // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
     // Update is called once per frame
+
     void Update()
     {
         theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.velocity.y);
